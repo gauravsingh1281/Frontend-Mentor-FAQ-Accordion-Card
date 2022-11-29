6158f45d3b0182e7330f71f19dbc88b1b50287b0
Gaurav Pratap Singh <gs492979@gmail.com>
2022-11-29 17:14:11 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+$("#answer-arrow1").click(function () {
+  $(".answer1").toggle("hidden");
+  $(".ques1").toggleClass("bold");
+  $("#answer-arrow1").toggleClass("rotate");
+});
+
+$("#answer-arrow2").click(function () {
+  $(".answer2").toggle("hidden");
+  $(".ques2").toggleClass("bold");
+  $("#answer-arrow2").toggleClass("rotate");
+});
+
+$("#answer-arrow3").click(function () {
+  $(".answer3").toggle("hidden");
+  $(".ques3").toggleClass("bold");
+  $("#answer-arrow3").toggleClass("rotate");
+});
+
+$("#answer-arrow4").click(function () {
+  $(".answer4").toggle("hidden");
+  $(".ques4").toggleClass("bold");
+  $("#answer-arrow4").toggleClass("rotate");
+});
+
+$("#answer-arrow5").click(function () {
+  $(".answer5").toggle("hidden");
+  $(".ques5").toggleClass("bold");
+  $("#answer-arrow5").toggleClass("rotate");
+});
